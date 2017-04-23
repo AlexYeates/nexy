49bb0255f3e9fc02e431fc10b7fb50227da420e5
AlexYeates <AlexYeates@hotmail.com>
2017-04-23 15:36:49 +0100

    Hitting error when looping through data

--- src/js/client.js
+++ src/js/client.js
@@ -9,6 +9,10 @@ function newsApi() {
   .get('https://newsapi.org/v1/articles?source=techcrunch&apiKey=e791d42519134d8ca50ff49ea0b3d33a')
   .done(data => {
     console.log(data);
+    var articleArray = data;
+    articleArray.forEach(function(element) {
+      console.log(element);
+    });
   });
 }
 
