ab8b3ee458cebc65661eb1b344d71bd26153f522
AlexYeates <AlexYeates@hotmail.com>
2017-04-23 15:27:58 +0100

    Test log of data coming from news api

--- src/js/client.js
+++ src/js/client.js
@@ -5,7 +5,11 @@ function init() {
 }
 
 function newsApi() {
-
+  $
+  .get('https://newsapi.org/v1/articles?source=techcrunch&apiKey=e791d42519134d8ca50ff49ea0b3d33a')
+  .done(data => {
+    console.log(data);
+  });
 }
 
 // news api key: e791d42519134d8ca50ff49ea0b3d33a
