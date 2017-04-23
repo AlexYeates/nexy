133034162c79bcb54e3fc73f75f4d2be1a743555
AlexYeates <AlexYeates@hotmail.com>
2017-04-23 13:20:39 +0100

    Updated routes for authentication

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,42 @@
-const express             = require('express');
-const router              = express.Router();
-const highlightController = require('../controllers/highlight');
+const express                = require('express');
+const router                 = express.Router();
+const highlightController    = require('../controllers/highlight');
+const registrationController = require('../controllers/registerations');
+const sessionsController     = require('../controllers/sessions');
+
+function secureRoute(req, res, next) {
+  if (!req.session.userId) {
+    return req.session.regenerate(() => {
+      req.flash('danger', 'You have to be loggin in to do that...');
+      req.redirect('/login');
+    });
+  }
+  next();
+}
 
 router.get('/', (req, res) => res.render('statics/home'));
 
 router.route('/highlight')
 .get(highlightController.index)
-.post(highlightController.create);
+.post(secureRoute, highlightController.create);
 router.route('highlight/new')
-.get(highlightController.new);
+.get(secureRoute, highlightController.new);
 router.route('/highlight/:id')
 .get(highlightController.show)
-.put(highlightController.update)
-.delete(highlightController.delete);
+.put(secureRoute, highlightController.update)
+.delete(secureRoute, highlightController.delete);
 router.route('/highlight/:id/edit')
-.get(highlightController.edit);
+.get(secureRoute, highlightController.edit);
+
+router.route('/register')
+.get(registrationController.new)
+.post(registrationController.create);
+
+router.route('/login')
+.get(sessionsController.new)
+.post(sessionsController.create);
+
+router.route('/logout')
+.get(sessionsController.delete);
 
 module.exports = router;
