cc37b791f3f6c90825a0b7b935657418e6118185
AlexYeates <AlexYeates@hotmail.com>
2017-04-23 12:51:24 +0100

    Updated routes file with highlight controller

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,19 @@
-const express = require('express');
-const router  = express.Router();
+const express             = require('express');
+const router              = express.Router();
+const highlightController = require('../controllers/highlight');
 
 router.get('/', (req, res) => res.render('statics/home'));
+
+router.route('/highlight')
+.get(highlightController.index)
+.post(highlightController.create);
+router.route('highlight/new')
+.get(highlightController.new);
+router.route('/highlight/:id')
+.get(highlightController.show)
+.put(highlightController.update)
+.delete(highlightController.delete);
+router.route('/highlight/:id/edit')
+.get(highlightController.edit);
 
 module.exports = router;
