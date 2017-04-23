70c83b849dcc073296511184b691b72c2714b799
AlexYeates <AlexYeates@hotmail.com>
2017-04-23 13:48:23 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express         = require('express');
+const morgan          = require('morgan');
 const expressLayouts  = require('express-ejs-layouts');
 const bodyParser      = require('body-parser');
 const mongoose        = require('mongoose');
@@ -16,6 +17,7 @@ mongoose.connect(env.db);
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
+app.use(morgan('dev'));
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +28,37 @@ app.use(methodOverride((req) => {
     return method;
   }
 }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'sssshhh',
+  resave: false,
+  saveUninitialised: false
+}));
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.userId) return next();
+
+  User
+  .findById(req.session.userId)
+  .exec()
+  .then(user => {
+    if (!req.session.userId) {
+      return req.session.regenerate(() => {
+        req.flash('danger', 'You have to loggin in to do that...');
+        res.redirect('/login');
+      });
+    }
+
+    res.locals.user = user;
+    res.locals.isLoggedIn = true;
+
+    console.log(user);
+
+    next();
+  });
+});
 
 app.use(router);
 
