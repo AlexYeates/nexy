b2eb15a8c21626e5f53eb1d93c85d7f24dae23cb
AlexYeates <AlexYeates@hotmail.com>
2017-04-24 14:50:51 +0100

    Given each news div their own ID

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,17 @@
 $(init);
 
 const newsSources = ['bbc-news', 'the-guardian-uk', 'associated-press', 'breitbart-news', 'cnn', 'mirror', 'the-economist', 'the-telegraph', 'reuters'];
+let newsId = 0;
 
 function init() {
   newsApi();
 }
 
 // news api key: e791d42519134d8ca50ff49ea0b3d33a
-
 newsSources.forEach(function(source) {
   newsApi(source);
 });
+
 
 function newsApi(source) {
   $
@@ -18,31 +19,14 @@ function newsApi(source) {
   .done(data => {
     var articleArray = data.articles;
     articleArray.forEach(function(element) {
-      $(`<div class="grid-item">
-        <img src="${element.urlToImage}">
-        <h2>${element.title}</h2>
-        <p>${element.description}</p>
-        <p><a href="${element.url}">Read more...</a></p>
-        <a class="btn btn-primary" href="/highlight/new">Highlight</a>
+      $(`<div class="grid-item" id="${newsId++}">
+      <img src="${element.urlToImage}">
+      <h2>${element.title}</h2>
+      <p>${element.description}</p>
+      <p><a href="${element.url}">Read more...</a></p>
+      <a class="btn btn-primary edit" href="/highlight/<%= highlight.id %>">Highlight</a>
       </div>`)
       .appendTo('#news');
     });
   });
 }
-
-// $
-// .get('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=e791d42519134d8ca50ff49ea0b3d33a')
-// .done(data => {
-//   var articleArray = data.articles;
-//   articleArray.forEach(function(element) {
-//     $(`<div><img src="${element.urlToImage}"><h2>${element.title}</h2><p>${element.description}</p><p><a href="${element.url}">Read more...</a></p></div>`).appendTo('#news');
-//   });
-//   // instead of appending, maybe push to array first?
-//   // would have to look at the url bit to see if its from the politics section, before pushing to array, so use a boolean
-// });
-// then have another api request here and repeat?
-// then at the end do a math.random to print to the screen?
-
-
-
-// note from nat
