d4ab677c18c3d53a78a102ee5fe7bd02540baf51
AlexYeates <AlexYeates@hotmail.com>
2017-04-24 10:19:21 +0100

    Using an array and forEach to get multiple requests from news API

--- src/js/client.js
+++ src/js/client.js
@@ -1,25 +1,37 @@
 $(init);
 
+const newsSources = ['bbc-news', 'sky-news'];
+
 function init() {
   newsApi();
 }
 
-function newsApi() {
+// news api key: e791d42519134d8ca50ff49ea0b3d33a
+
+newsSources.forEach(function(source) {
+  newsApi(source);
+});
+
+function newsApi(source) {
   $
-  .get('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=e791d42519134d8ca50ff49ea0b3d33a')
+  .get(`https://newsapi.org/v1/articles?source=${source}&sortBy=top&apiKey=e791d42519134d8ca50ff49ea0b3d33a`)
   .done(data => {
-    console.log(data);
     var articleArray = data.articles;
     articleArray.forEach(function(element) {
-      console.log(element.title);
-      console.log(element.description);
-      console.log(element.author);
-      console.log(element.publishedAt);
-      console.log(element.url);
-      console.log(element.urlToImage);
       $(`<div><img src="${element.urlToImage}"><h2>${element.title}</h2><p>${element.description}</p><p><a href="${element.url}">Read more...</a></p></div>`).appendTo('#news');
     });
   });
 }
 
-// news api key: e791d42519134d8ca50ff49ea0b3d33a
+// $
+// .get('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=e791d42519134d8ca50ff49ea0b3d33a')
+// .done(data => {
+//   var articleArray = data.articles;
+//   articleArray.forEach(function(element) {
+//     $(`<div><img src="${element.urlToImage}"><h2>${element.title}</h2><p>${element.description}</p><p><a href="${element.url}">Read more...</a></p></div>`).appendTo('#news');
+//   });
+//   // instead of appending, maybe push to array first?
+//   // would have to look at the url bit to see if its from the politics section, before pushing to array, so use a boolean
+// });
+// then have another api request here and repeat?
+// then at the end do a math.random to print to the screen?
