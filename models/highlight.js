e91c7aae72850758f08378431d6c4ac78ae7cd4a
AlexYeates <AlexYeates@hotmail.com>
2017-04-24 12:08:39 +0100

    Added content to index.ejs page

--- models/highlight.js
+++ models/highlight.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const highlightSchema = new mongoose.Schema({
-  headline: { type: String, required: true },
+  urlToImage: { type: String, required: true },
+  title: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
-  readMore: { type: String, required: true }
+  url: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Highligh', highlightSchema);
